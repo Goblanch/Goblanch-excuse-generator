f9d597ec0ae35082b7c8326ca673cdcefdbffa67
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2024-09-02 10:59:23 +0000

    finish excuse generator

--- src/app.js
+++ src/app.js
@@ -5,8 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Excuse variables
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
-  let who = ["A"];
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  document.getElementById("excuse").innerHTML = excuse;
+  console.log(excuse);
 };
+
+function generateExcuse() {
+  let excuse = who[getRandomIndex(0, who.length)];
+  excuse = excuse + " " + action[getRandomIndex(0, action.length)];
+  excuse = excuse + " " + what[getRandomIndex(0, what.length)];
+  excuse = excuse + " " + when[getRandomIndex(0, when.length)];
+
+  return excuse;
+}
+
+function getRandomIndex(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
